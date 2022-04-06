06706b7a777f71bbe1c7c93cc6ffaff09b96d4ce
ceren.mert <ceren1481@hotmail.com>
2022-04-06 13:59:10 +0300

    added new cases

--- cypress/integration/app-tr.spec.jsx
+++ cypress/integration/app-tr.spec.jsx
@@ -4,7 +4,7 @@ describe('Playing superpeer.com content', () => {
     });
 
     it('Search for “Superpeer”', () => {
-        cy.get('input[name="search_query"]').type('Superpeer{enter}');
+        cy.get('input[name="search_query"]').type('Superpeer{enter}').wait(5000);
     });
 
     it('Click and open "Superpeer\'in kuruluş hikayesi" content', () => {
@@ -16,11 +16,44 @@ describe('Playing superpeer.com content', () => {
     });
 
     it('Check the title', () => {
-        cy.get('h1.title').contains('Superpeer\'in kuruluş hikayesi');
+        cy.get('h1.title').wait(6000).contains('Superpeer\'in kuruluş hikayesi');
     });
 
     it('Check the video date', () => {
         cy.get('#info-strings').contains('11 Ara 2020');
+    });
+
+    xit('Video duration is as expected', () => {
+        cy.get('.ytp-time-duration').invoke('text').should('be.equal','1:49:51');
+    });
+
+    it('Check the comment count visibility and count is greater than 10', () => {
+        cy.scrollTo('bottom', {duration: 200}).wait(2000).scrollTo('bottom', {duration: 200})
+            .get('div#title  yt-formatted-string > span').first()
+            .then(console.log)
+            .should('be.visible')
+            .invoke('text')
+            .then(parseInt).should('be.gt', 10);
+    });
+
+    it('Check the author of third comment', () => {
+        //   cy.get('ytd-comment-thread-renderer:nth-of-type(3) > ytd-comment-renderer#comment a#author-text > .style-scope.ytd-comment-renderer')
+        cy.get('ytd-comment-thread-renderer').eq(3).get('ytd-comment-renderer#comment a#author-text')
+            .contains('Senariturk');
+    });
+
+    it('check the subscriber count' , () => {
+        cy.get('yt-formatted-string#owner-sub-count').invoke('text')
+            .then(console.log)
+            .then(x => {
+                return x.replace('B abone','').replace(',','.').trim();
+            })
+            .then(console.log)
+            .then(parseFloat)
+            .then(console.log)
+            .then(x => x * 1000)
+            .then(console.log)
+            .should('be.equal',15100)
     });
 
     it('Check the video owner', () => {
@@ -34,7 +67,7 @@ describe('Playing superpeer.com content', () => {
     });
 
     it('Stop the video', () => {
-        cy.get('button.ytp-play-button').click();
+        cy.get('button.ytp-play-button.ytp-button').click();
     });
 
     it('Scroll down and check the search bar', () => {
@@ -48,7 +81,7 @@ describe('Playing superpeer.com content', () => {
             const randomVideoIndex = Math.floor(Math.random() * (x.length - 1));
 
             cy.get('#items h3')
-                .eq(randomVideoIndex)
+                .eq(randomVideoIndex) // Get A DOM element at a specific index in an array of elements
                 .invoke('text')
                 .then(y => {
                     videoTitle = y.trim()
@@ -63,4 +96,17 @@ describe('Playing superpeer.com content', () => {
                 });
         });
     });
+
+    it('Button is red and then click on the button, and check the text after click the subscribe button', () => {
+        cy.get('div#subscribe-button > .size-default.style-destructive.style-scope.ytd-video-secondary-info-renderer')
+            .should('have.css','background-color','rgb(204, 0, 0)')
+            .click()
+            .then(() => {
+            cy.get('.style-scope.ytd-popup-container > yt-formatted-string#title')
+                .invoke('text')
+                .should('be.equal', 'Bu kanala abone olmak istiyor musunuz?');
+        });
+    });
+
+
 });
--- cypress/integration/app.spec.jsx
+++ cypress/integration/app.spec.jsx
@@ -27,6 +27,7 @@ describe('Playing superpeer.com content', () => {
 
     it('Check the title', () => {
         cy.get('h1.title').contains('Superpeer\'in kuruluş hikayesi');
+        cy.get('.title .ytd-video-primary-info-renderer').contains('Superpeer\'in kuruluş hikayesi');
     });
 
     it('Check the video date', () => {
@@ -37,14 +38,48 @@ describe('Playing superpeer.com content', () => {
         cy.get('#text-container.ytd-channel-name').contains('Fatih Acet');
     });
 
+    xit('Video duration is as expected', () => {
+        cy.get('.ytp-time-duration').invoke('text').should('be.equal','1:49:51');
+    });
+
+    it('Check the comment count visibility and count is greater than 10', () => {
+        cy.scrollTo('bottom', {duration: 200}).wait(2000).scrollTo('bottom', {duration: 200})
+            .get('div#title  yt-formatted-string > span').first()
+            .then(console.log)
+            .should('be.visible')
+            .invoke('text')
+            .then(parseInt).should('be.gt', 10);
+    });
+
+    it('Check the author of third comment', () => {
+        cy.get('ytd-comment-thread-renderer').eq(3).get('ytd-comment-renderer#comment a#author-text')
+            .contains('Senariturk');
+    });
+
+    it('check the subscriber count' , () => {
+        cy.get('yt-formatted-string#owner-sub-count').invoke('text')
+            .then(console.log)
+            .then(x => {
+                return x.replace('B abone','').replace(',','.').trim();
+            })
+            .then(console.log)
+            .then(parseFloat)
+            .then(console.log)
+            .then(x => x * 1000)
+            .then(console.log)
+            .should('be.equal',15100)
+    });
+
+
     it('Check the video view greater than 100', () => {
-        cy.get('#info .view-count').invoke('text').then(x => {
-            return x.replace(' views', '').replace(',', '');
-        }).then(parseInt).should('be.gt', 100);
+        cy.get('#info .view-count').invoke('text')
+            .then(x => { return x.replace(' views', '')
+                .replace(',', '');})
+            .then(parseInt).should('be.gt', 100);
     });
 
     it('Stop the video', () => {
-        cy.get('button.ytp-play-button').click();
+        cy.wait(2000).get('button.ytp-play-button.ytp-button').click();
     });
 
     it('Scroll down and check the search bar', () => {
@@ -73,4 +108,18 @@ describe('Playing superpeer.com content', () => {
                 });
         });
     });
+
+    it('Button is red and then click on the button, and check the text after click the subscribe button', () => {
+        const text = "Want to subscribe to this channel?";
+        cy.get('div#subscribe-button > .size-default.style-destructive.style-scope.ytd-video-secondary-info-renderer')
+            .should('have.css','background-color','rgb(204, 0, 0)')
+            .click()
+            .then(() => {
+                cy.get('.style-scope.ytd-popup-container > yt-formatted-string#title')
+                .invoke('text')
+                .should('be.equal', text);
+        });
+    });
+
+
 });
